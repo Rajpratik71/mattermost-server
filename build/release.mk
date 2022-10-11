a0e2421c59e37d3ff4ec61fbdb94ee9e327bc73b
Harrison Healey <harrisonmhealey@gmail.com>
2022-10-11 11:09:12 -0400

    potentially fix variables and add insulting logs

--- build/release.mk
+++ build/release.mk
@@ -194,17 +194,28 @@ endif
 
 	@# Products
 	BOARDS_WEBAPP_DIR=$(BUILD_BOARDS_DIR)/mattermost-plugin/webapp/dist
-	echo HARRISON getting products from $$BOARDS_WEBAPP_DIR if it exists
+	echo HARRISON getting products from $(BOARDS_WEBAPP_DIR) if it exists
 	echo HARRISON and copying it to $(DIST_PATH_GENERIC)/products
+
+	echo HARRISON printing boards stuff
+	echo ""
+	ls $(BOARDS_WEBAPP_DIR)
+	echo ""
+
 	@if [ -f $(BUILD_BOARDS_DIR)/ ] ; then \
 		echo HARRISON that dir exists; \
 		echo "Copied web app files for Boards product"; \
 		mkdir -p $(DIST_PATH_GENERIC)/products/boards; \
-		cp -R $$BOARDS_WEBAPP_DIR/* $(DIST_PATH_GENERIC)/products/boards/; \
+		cp -R $(BOARDS_WEBAPP_DIR)/* $(DIST_PATH_GENERIC)/products/boards/; \
 		ls $(DIST_PATH_GENERIC)/products/boards; \
 	else \
 		echo HARRISON that dir doesnt exist sad face; \
 	fi
+
+	echo HARRISON printing productgs stuff
+	echo ""
+	tree $(BOARDS_WEBAPP_DIR) || echo "tree doesnt exist, idiot" && ls $(DIST_PATH_GENERIC)/products
+	echo ""
 
 package-osx-amd64: package-prep
 	DIST_PATH_GENERIC=$(DIST_PATH_OSX_AMD64) CURRENT_PACKAGE_ARCH=darwin_amd64 PLUGIN_ARCH=osx-amd64 MMCTL_PLATFORM="Darwin-x86_64" MM_BIN_NAME=mattermost $(MAKE) package-general
